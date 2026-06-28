87a363b04bb1cd0d5316462a61c3e204a0b13542
ΡΟΖΑΡΙΟ ΑΛΕΞΑΝΔΡΟΣ <rosandrox12@gmail.com>
2026-06-28 22:21:56 +0300

    Update Axel_Brain.ino

--- Axel_Brain.ino
+++ Axel_Brain.ino
@@ -1,7 +1,8 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
-#define PIR_PIN 2
+// PIN DEFINITIONS
+#define LDR_PIN A0
 #define TRIG_PIN 5
 #define ECHO_PIN 4
 #define SPEAKER_PIN 3
@@ -14,9 +15,42 @@
 #define LCD_D6 11
 #define LCD_D7 12
 
+// OBJECTS 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 Servo neck;
 
+byte eye_one[8] = {
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B00000,
+  B00000
+};
+byte backslash[8] = {
+  0b00000,
+  0b10000,
+  0b01000,
+  0b00100,
+  0b00010,
+  0b00001,
+  0b00000,
+  0b00000
+};
+byte magnifyingGlass[8] = {
+  0b11110,
+  0b10010,
+  0b10010,
+  0b11110,
+  0b00001,
+  0b00001,
+  0b00001,
+  0b00000
+};
+
+// VARIABLES ECC.
 unsigned long lastMotionTime = 0;
 unsigned long lastEmotionTime = 0;
 const unsigned long TIMEOUT = 300000; 
@@ -26,122 +60,232 @@ bool scared = false;
 bool rageQuit = false;
 int scareCount = 0;
 
-String facts[] = {
-  "Robots can learn",
-  "First bot: 1954",
-  "Mars rover rocks",
-  "Bots need code",
-  "Servo is precise"
-};
+const int LIGHT_THRESHOLD = 450;   //Adjust
 
-byte eye_one[8] = {B00000, B00000, B11111, B11111, B11111, B11111, B00000, B00000};
-byte backslash[8] = {0b00000, 0b10000, 0b01000, 0b00100, 0b00010, 0b00001, 0b00000, 0b00000};
-byte magnifyingGlass[8] = {0b11110, 0b10010, 0b10010, 0b11110, 0b00001, 0b00001, 0b00001, 0b00000};
-
+// SETUP 
 void setup() {
-  pinMode(PIR_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(SPEAKER_PIN, OUTPUT);
+
   neck.attach(SERVO_PIN);
   neck.write(90);
+
   randomSeed(analogRead(0));
+
   lcd.begin(16, 2);
   lcd.createChar(0, eye_one);
   lcd.createChar(1, backslash); 
   lcd.createChar(2, magnifyingGlass); 
-  
-  lcd.print("Robot Booting");
-  delay(1500);
   lcd.clear();
+
+  Serial.begin(9600);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Robot Booting");
+  tone(SPEAKER_PIN, 440, 100); delay(150);
+  tone(SPEAKER_PIN, 554, 100); delay(150);
+  tone(SPEAKER_PIN, 659, 200); delay(250);
+  delay(1000);
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
   lcd.print("Soldering at 2am");
   lcd.setCursor(0, 1);
   lcd.print("is fire...");
   delay(3000);
+  lcd.clear();
+
   idleFace();
   lastMotionTime = millis();
   lastEmotionTime = millis();
 }
 
+// LOOP 
 void loop() {
+
   long dist = getDistance();
-  if (digitalRead(PIR_PIN) == HIGH && !rageQuit) {
-    lastMotionTime = millis();
-    searching = false;
+  int lightLevel = analogRead(LDR_PIN);
+
+  Serial.print("Light: ");
+  Serial.println(lightLevel);
+
+  // Someone nearby?
+  if (dist > 0 && dist < 30 && !rageQuit) {
+      lastMotionTime = millis();
+      searching = false;
   }
-  if (!searching && !rageQuit && millis() - lastMotionTime > TIMEOUT) {
-    searching = true;
-    lcd.clear();
-    lcd.print("SEARCHING...");
+
+  // Nobody seen for TIMEOUT
+  if (!searching &&
+      !rageQuit &&
+      millis() - lastMotionTime > TIMEOUT) {
+
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("SEARCHING...");
+
+      neck.write(90);
+
+      tone(SPEAKER_PIN,1000,100);
+      delay(200);
+      tone(SPEAKER_PIN,1500,100);
+      delay(800);
+
+      lcd.clear();
+
+      searching = true;
   }
-  if (searching) {
-    searchForPerson();
-  } else if (!rageQuit) {
-    if (dist > 0 && dist < 10) {
-      if (!scared) {
-        scared = true;
-        scareCount++;
-        if (scareCount >= 4) rageQuitMode();
-        else isScared();
+
+  if(searching){
+      searchForPerson();
+      return;
+  }
+
+  // Too close -> scared
+  if(dist > 0 && dist < 10){
+
+      if(rageQuit){
+
+          rageQuit = false;
+          scareCount = 0;
+
+          lcd.clear();
+          lcd.setCursor(2,0);
+          lcd.print("FINE IM BACK");
+
+          neck.write(90);
+
+          tone(SPEAKER_PIN,800,200);
+          delay(300);
+          tone(SPEAKER_PIN,1200,300);
+
+          delay(3000);
+
+          idleFace();
+
+          lastEmotionTime = millis();
       }
+
+      else if(!scared){
+
+          scared = true;
+          scareCount++;
+
+          if(scareCount >= 4)
+              rageQuitMode();
+          else
+              isScared();
+      }
+
       lastEmotionTime = millis();
-    } else {
+  }
+
+  else{
+
       scared = false;
-      if (millis() - lastEmotionTime > 12000) {
-        int action = random(0, 4);
-        if (action == 0) isBored();
-        else if (action == 1) isHappy();
-        else if (action == 2) isAngry();
-        else sayFact();
-        lastEmotionTime = millis();
+
+      // NEW DARK MODE
+      if(lightLevel > LIGHT_THRESHOLD){
+
+          isDarkScared();
+          lastEmotionTime = millis();
       }
-    }
-  } else if (rageQuit && dist < 10) {
-    rageQuit = false;
-    scareCount = 0;
-    lcd.clear();
-    lcd.print("FINE. IM BACK.");
-    delay(2000);
-    idleFace();
+
+      else if(!rageQuit &&
+              millis() - lastEmotionTime > 12000){
+
+          int emotion = random(0,3);
+
+          if(emotion==0)
+              isBored();
+
+          else if(emotion==1)
+              isHappy();
+
+          else
+              isAngry();
+
+          lastEmotionTime = millis();
+      }
   }
 }
 
+// ULTRASONIC FUNCTION
 long getDistance() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
+
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-  return pulseIn(ECHO_PIN, HIGH) * 0.034 / 2;
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  long distance = duration * 0.034 / 2;
+
+  return distance;
 }
 
+// FUNCTIONS
 void searchForPerson() {
-  lcd.setCursor(6, 0); lcd.write(byte(0)); lcd.setCursor(8, 0); lcd.write(byte(0));
-  lcd.setCursor(6, 1); lcd.print("___"); lcd.setCursor(9, 1); lcd.write(2);
-  if (getDistance() > 0 && getDistance() < 30) foundPerson();
+  lcd.setCursor(6, 0);
+  lcd.write(byte(0));
+  lcd.setCursor(8, 0);
+  lcd.write(byte(0));
+  lcd.setCursor(6, 1);
+  lcd.print("___");
+  lcd.setCursor(9, 1);
+  lcd.write(2);
+
+  long dist = getDistance();
+
+  if (dist > 0 && dist < 30) {
+    foundPerson();
+    return;
+  }
 }
 
 void foundPerson() {
   searching = false;
-  lcd.clear(); lcd.print("WELCOME BACK");
+
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("WELCOME BACK");
   neck.write(90);
+  
   tone(SPEAKER_PIN, 600, 150); delay(200);
   tone(SPEAKER_PIN, 800, 150); delay(200);
   tone(SPEAKER_PIN, 1200, 300);
+  
   delay(3000);
+  lcd.clear();
   idleFace();
   lastEmotionTime = millis();
 }
 
 void idleFace() {
   neck.write(90);
-  lcd.clear(); lcd.setCursor(6, 0); lcd.write(byte(0)); lcd.setCursor(8, 0); lcd.write(byte(0));
-  lcd.setCursor(6, 1); lcd.write(byte(1)); lcd.setCursor(7, 1); lcd.print("_/");
+  lcd.clear();
+  lcd.setCursor(6, 0);
+  lcd.write(byte(0));
+  lcd.setCursor(8, 0);
+  lcd.write(byte(0));
+  lcd.setCursor(6, 1);
+  lcd.write(byte(1));
+  lcd.setCursor(7, 1);
+  lcd.print("_/");
 }
 
 void isScared() {
-  lcd.clear(); lcd.setCursor(6, 0); lcd.write(byte(0)); lcd.setCursor(8, 0); lcd.write(byte(0));
-  lcd.setCursor(7, 1); lcd.print("O");
+  lcd.clear();
+  lcd.setCursor(6, 0);
+  lcd.write(byte(0));
+  lcd.setCursor(8, 0);
+  lcd.write(byte(0));
+  lcd.setCursor(7, 1);
+  lcd.print("O");
+  
   for(int i = 0; i < 6; i++) {
     neck.write(i % 2 == 0 ? 120 : 60);
     tone(SPEAKER_PIN, 2000, 50);
@@ -153,64 +297,125 @@ void isScared() {
 }
 
 void isAngry() {
-  lcd.clear(); lcd.setCursor(2, 0); lcd.print("Type faster"); lcd.setCursor(6, 1); lcd.print(">_<");
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Type faster");
+  lcd.setCursor(6, 1);
+  lcd.print(">_<");
+  
   if (random(0, 2) == 0) {
     for(int i = 0; i < 7; i++) {
-      neck.write(135); tone(SPEAKER_PIN, 165, 200); delay(200);
-      neck.write(45); tone(SPEAKER_PIN, 175, 200); delay(200);
+      int speed = 450 - (i * 55);
+      neck.write(135);
+      tone(SPEAKER_PIN, 165, speed/2);
+      delay(speed);
+      neck.write(45);
+      tone(SPEAKER_PIN, 175, speed/2);
+      delay(speed);
     }
   } else {
     for(int i = 0; i < 16; i++) {
       neck.write(i % 2 == 0 ? 110 : 70);
       tone(SPEAKER_PIN, 1100, 50);
-      delay(max(50, 500 - (i * 30)));
+      int delayTime = 500 - (i * 30);
+      if(delayTime < 50) delayTime = 50;
+      delay(delayTime);
     }
-    tone(SPEAKER_PIN, 1200, 1200); delay(1200);
+    neck.write(90);
+    tone(SPEAKER_PIN, 1200, 1200); 
+    delay(1200);
   }
+  
   noTone(SPEAKER_PIN);
   delay(3000);
   idleFace();
 }
 
 void isBored() {
-  lcd.clear(); lcd.setCursor(2, 0); lcd.print("Start typing"); lcd.setCursor(6, 1); lcd.print("-_-");
-  neck.write(135); tone(SPEAKER_PIN, 300, 500); delay(600);
-  neck.write(45); tone(SPEAKER_PIN, 200, 800);
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Start typing");
+  lcd.setCursor(6, 1);
+  lcd.print("-_-");
+  
+  neck.write(135);
+  tone(SPEAKER_PIN, 300, 500);
+  delay(600);
+  neck.write(45);
+  tone(SPEAKER_PIN, 200, 800);
+  
   delay(4500);
   idleFace();
 }
 
 void isHappy() {
-  lcd.clear(); lcd.setCursor(4, 0); lcd.print("GOOD JOB"); lcd.setCursor(6, 1); lcd.print("^_^");
+  lcd.clear();
+  lcd.setCursor(4, 0);
+  lcd.print("GOOD JOB");
+  lcd.setCursor(6, 1);
+  lcd.print("^_^");
+  
   int melody[] = { 330, 330, 349, 392, 392, 349, 330, 294, 262, 262, 294, 330, 330, 294, 294 };
+  int durations[] = { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 8, 2 };
+  
   for (int i = 0; i < 15; i++) {
     neck.write(i % 2 == 0 ? 110 : 70);
-    tone(SPEAKER_PIN, melody[i], 250);
-    delay(300);
+    int duration = 1000 / durations[i];
+    tone(SPEAKER_PIN, melody[i], duration);
+    delay(duration * 1.30);
+    noTone(SPEAKER_PIN);
   }
+  
   neck.write(90);
   delay(3000);
-  idleFace();
-}
-
-void sayFact() {
-  lcd.clear(); lcd.setCursor(0, 0); lcd.print("Robot Fact:");
-  lcd.setCursor(0, 1); lcd.print(facts[random(0, 5)]);
-  for(int i = 0; i < 3; i++) {
-    neck.write(45); delay(500); neck.write(135); delay(500);
-  }
-  neck.write(90);
-  delay(2000);
   idleFace();
 }
 
 void rageQuitMode() {
   rageQuit = true;
-  lcd.clear(); lcd.print("Thats absolute"); lcd.setCursor(4, 1); lcd.print("ragebait");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Thats absolute");
+  lcd.setCursor(4, 1);
+  lcd.print("ragebait");
+  
   neck.write(180);
+  
   tone(SPEAKER_PIN, 150, 400); delay(500);
   tone(SPEAKER_PIN, 100, 800); delay(1500);
-  lcd.clear(); lcd.setCursor(2, 0); lcd.print("I am leaving");
+  
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("I am leaving");
   delay(3000);
   lcd.clear(); 
+}
+
+//Dark
+void isDarkScared() {
+
+  lcd.clear();
+
+  lcd.setCursor(2,0);
+  lcd.print("TOO DARK!");
+
+  lcd.setCursor(0,1);
+  lcd.print("Turn on light");
+
+  for(int i=0;i<4;i++){
+
+      neck.write(70);
+      tone(SPEAKER_PIN,1700,80);
+      delay(180);
+
+      neck.write(110);
+      tone(SPEAKER_PIN,2000,80);
+      delay(180);
+  }
+
+  neck.write(90);
+
+  delay(2500);
+
+  idleFace();
 }
